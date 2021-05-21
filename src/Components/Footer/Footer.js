a10d1eb9dd4eb9a010ed24dd01f1eca53e36caa8
farhan-nahid <nahidahmed9933@gmail.com>
2021-05-21 15:10:04 +0600

    update user name

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="text-center bg-light">
-             <small>Designed & Build by <a href="https://www.facebook.com/farhan.nahid.01" target="blank">Farhan Ahmed Nahid</a></small> <br />
+             <small>Designed & Build by <a href="https://www.facebook.com/dev.farhanNahid" target="blank">Farhan Ahmed Nahid</a></small> <br />
             <small>{(new Date()).getFullYear()} &copy; copyright | Farhan</small>     
         </footer>
     );
